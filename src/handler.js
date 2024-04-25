86fc87134a6918bcbece4af781986f5c1903acfd
Ryzan87 <rizkyseptian56511@gmail.com>
2024-04-25 19:09:24 +0700

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,63 @@
 const notes = require('./notes');
 
+const addNote = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+const getAllNotes = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteById = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+
 const updateNote = (request, h) => {
   const noteId = request.params.id;
   const updateData = request.payload;
@@ -34,7 +92,7 @@ const deleteNote = (request, h) => {
       message: 'Catatan berhasil dihapus',
     };
   }
-
+  
   return {
     status: 'fail',
     message: 'Catatan gagal dihapus. Id catatan tidak ditemukan',
@@ -43,7 +101,9 @@ const deleteNote = (request, h) => {
 
 
 module.exports = {
-  // ... previous handlers
   updateNote,
   deleteNote,
+  addNote,
+  getAllNotes,
+  getNoteById,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
 const { updateNote, deleteNote } = require('./handler');
 
 const routes = [
-  // {
-  //   method: 'POST',
-  //   path: '/notes',
-  //   handler: notesHandler.addNote,
-  // },
-  // {
-  //   method: 'GET',
-  //   path: '/notes',
-  //   handler: notesHandler.getAllNotes,
-  // },
-  // {
-  //   method: 'GET',
-  //   path: '/notes/{id}',
-  //   handler: notesHandler.getNoteById,
-  // },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNote,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotes,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteById,
+  },
   {
     method: 'PUT',
     path: '/notes/{id}',
